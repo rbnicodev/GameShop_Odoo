b1caa74c667beb9dce4296b808e33bdfcb072264
Rubén Muñoz <rubenmunoz.alu@iespacomolla.es>
2022-02-16 08:33:13 +0100

    añadido campo calculado "Plataformas disponibles"

--- models/game_model.py
+++ models/game_model.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Game_Model(models.Model):
         _name = "game.model"
@@ -13,3 +13,9 @@ class Game_Model(models.Model):
         price = fields.Float('Precio de venta', required = True, help = 'Precio')
         genre_ids = fields.Many2many('genre.model' , string="Género")
         platform_ids = fields.Many2many('platform.model', string="Plataforma")
+        available_platforms = fields.Integer(compute='_compute_available_platforms', string="Plataformas disponibles")
+        
+        def _compute_available_platforms(self):
+                self.available_platforms = 0
+                for platform in self.platform_ids:
+                        self.available_platforms += 1
