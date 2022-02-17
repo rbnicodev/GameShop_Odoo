0deb5d0a4c649c116ab03400f8c9d45c63683b00
Rubén Muñoz <rubenmunoz.alu@iespacomolla.es>
2022-02-17 22:35:57 +0100

    ahora funsiona :D

--- models/game_model.py
+++ models/game_model.py
@@ -10,17 +10,27 @@ class Game_Model(models.Model):
         description = fields.Text('Descripción', required = True, help = 'Descripcion')
         cover = fields.Binary()
         date_availability = fields.Date('Disponibilidad', required = True, help = 'Fechas disponibles')
+        #precio de venta
         price = fields.Float('Precio de venta', required = True, help = 'Precio')
         starting_price = fields.Float('Precio de salida', required = False, help = 'Precio original')
-        revaluation = fields.Float(compute='_compute_revaluation', string='Revalorización (%)')
+        
         genre_ids = fields.Many2many('genre.model' , string="Género")
         platform_ids = fields.Many2many('platform.model', string="Plataforma")
+        
+        revaluation = fields.Float(compute='_compute_revaluation', string='Revalorización (%)')
         available_platforms = fields.Integer(compute='_compute_available_platforms', string="Plataformas disponibles")
         
+        @api.onchange("platform_ids")
         def _compute_available_platforms(self):
-                self.available_platforms = 0
-                for platform in self.platform_ids:
-                        self.available_platforms += 1
-                        
+                for record in self:
+                        record.available_platforms = 0
+                for n in range(len(record.platform_ids)):
+                        record.available_platforms += 1
+        
+        @api.onchange("price", "starting_price")               
         def _compute_revaluation(self):
-                self.revaluation = self.price / self.starting_price
+                for record in self:
+                        if record.starting_price > 0:
+                                record.revaluation = record.price / record.starting_price
+                        else:
+                                record.revaluation = 0
